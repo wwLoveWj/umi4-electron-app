8aac005384ee1ab8e664c02e3b45516d15fb6d8d
wwLoveWj <blww885@163.com>
2025-06-26 20:42:26 +0800

    feat: 全局增加进度动画

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,8 @@ import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 dayjs.locale("zh-cn");
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
@@ -27,4 +29,16 @@ export function rootContainer(container: React.ReactNode) {
 
 export async function render(oldRender: any) {
   oldRender();
+}
+
+/**
+ * Umi4 路由切换时触发进度条动画
+ * @param param0 路由切换参数
+ */
+export function onRouteChange({ location, routes, action, history }: any) {
+  NProgress.start();
+  // 500ms 后自动完成，防止页面太快一闪而过
+  setTimeout(() => {
+    NProgress.done();
+  }, 500);
 }
